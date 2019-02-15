37f9d539a375e42f86b43ade9c85a77ff6fe49e2
cha1ra <cha1ra.garnet@gmail.com>
2019-02-15 15:55:28 +0900

    vuex test

--- pages/inspire.vue
+++ pages/inspire.vue
@@ -1,13 +1,12 @@
 <template>
   <v-layout>
     <v-flex text-xs-center>
-      <img
-        src="/v.png"
-        alt="Vuetify.js"
-        class="mb-5"
-      >
-      <blockquote class="blockquote">
-        &#8220;First, solve the problem. Then, write the code.&#8221;
+      <p>{{ $store.state.message }}</p>
+      <div class="link" @click="$store.commit('count')" @click.shift="$store.commit('reset')">
+        <a>clicked!: {{ $store.state.counter }}</a>
+      </div>
+      <img src="/v.png" alt="Vuetify.js" class="mb-5">
+      <blockquote class="blockquote">&#8220;First, solve the problem. Then, write the code.&#8221;
         <footer>
           <small>
             <em>&mdash;John Johnson</em>
@@ -17,3 +16,14 @@
     </v-flex>
   </v-layout>
 </template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      title: 'Hello',
+      message: 'this is message.'
+    }
+  }
+}
+</script>
